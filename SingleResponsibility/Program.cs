d7fb8f6bd8c03e1c69e9143657b186d8f53f0557
Serkan Topkan <stopkan@isbak.istanbul>
2022-04-19 08:57:06 +0300

    Single Responsibility eklendi

---
+++ SingleResponsibility/Program.cs
@@ -0,0 +1,43 @@
+﻿//Olmaması gereken
+/*public class Fabrika
+   {
+       public void TelefonUret() {
+           //bla bla
+       }
+       public void TelefonSat() { 
+           //bla bla
+       }
+   }*/
+
+//Olması gereken
+public class Fabrika
+{
+    public void TelefonUret()
+    {
+        //bla bla
+    }
+}
+public class Satis
+{
+    public void TelefonSat()
+    {
+        //bla bla
+
+        FaturaKes();
+    }
+
+    public void FaturaKes()
+    {
+
+    }
+}
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+
+    }
+}
