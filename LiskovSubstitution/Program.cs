5b16ce31a00e84a2c5d6b59f4d6af999e980ed40
Serkan Topkan <stopkan@isbak.istanbul>
2022-04-19 10:34:07 +0300

    Liskov Substitution kodları eklendi

---
+++ LiskovSubstitution/Program.cs
@@ -0,0 +1,59 @@
+﻿public abstract class Arac
+{
+    public abstract void Calistir()
+    {
+        //Çalıştı
+    }
+    public abstract void Durdur()
+    {
+        //Durdu
+    }
+
+}
+
+public class Araba : Arac
+{
+    public override void Calistir()
+    {
+        //Araba araç çalıştı
+    }
+    public override void Durdur()
+    {
+        //Araba araç durdu
+    }
+}
+
+public interface IUcanArac
+{
+    void FlapAc()
+    {
+        /* Flap Açıldı*/
+    }
+}
+public class Ucak : Arac, IUcanArac
+{
+
+    public override void Calistir()
+    {
+        //Uçak çalıştı
+    }
+    public override void Durdur()
+    {
+        //Uçak durdu
+    }
+    public void FlapAc()
+    {
+        //Flap açıldı
+    }
+}
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+
+    }
+}
